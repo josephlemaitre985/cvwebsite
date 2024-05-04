347a1c9eb2db314e8924861315ef2edb6c05071a
Joseph <josephlemaitre985@gmail.com>
2024-05-04 15:50:40 +0200

    maj app.js

--- app.js
+++ app.js
@@ -5,16 +5,9 @@ if (process.env.NODE_ENV === 'production') {
     require('dotenv').config({ path: './.env.development' });
 }
 
-// Logs pour débogage
-console.log('Environment:', process.env.NODE_ENV);
-console.log('Database Host:', process.env.DB_HOST);
-console.log('Database Port:', process.env.DB_PORT);
-console.log('Database Name:', process.env.DB_NAME);
-console.log('API URL:', process.env.REACT_APP_API_URL);
-
 const express = require('express');
 const userRoutes = require('./routes/userRoutes');
-const stripeRoutes = require('./routes/stripeRoutes'); // Importation des routes Stripe
+const stripeRoutes = require('./routes/stripeRoutes');
 const sequelize = require('./config/database');
 const cors = require('cors');
 
@@ -26,13 +19,23 @@ app.set('trust proxy', true);
 // Configuration CORS spécifique en fonction de l'environnement
 const corsOptions = {
     origin: process.env.NODE_ENV === 'production' 
-            ? 'https://cv-web-site-client-1663cb36180a.herokuapp.com'  // URL de production de votre front-end
-            : 'http://localhost:3000',  // URL de développement local de votre front-end
-    optionsSuccessStatus: 200
+            ? 'https://cv-web-site-client-1663cb36180a.herokuapp.com'
+            : 'http://localhost:3000',
+    optionsSuccessStatus: 200,
+    credentials: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: "Content-Type,Authorization"
 };
 
 app.use(cors(corsOptions));
 app.use(express.json());
+
+// Log de débogage pour chaque requête
+app.use((req, res, next) => {
+    console.log(`Requête reçue avec méthode ${req.method} de ${req.origin} avec les en-têtes suivants: ${JSON.stringify(req.headers)}`);
+    next();
+});
+
 app.use((req, res, next) => {
     res.header('Content-Type', 'application/json');
     next();
@@ -43,7 +46,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/users', userRoutes);
-app.use('/api/stripe', stripeRoutes); // Utilisation des routes Stripe
+app.use('/api/stripe', stripeRoutes);
 
 // Tester la connexion à la base de données avant de démarrer le serveur
 sequelize.authenticate()
