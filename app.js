fc31c41f721bdd7b90c7e77a4af2e0ef567345ee
Joseph <josephlemaitre985@gmail.com>
2024-05-05 15:40:31 +0200

    new app.js

--- app.js
+++ app.js
@@ -16,35 +16,42 @@ const port = process.env.PORT || 5000;
 
 app.set('trust proxy', true);
 
-// Configuration CORS spécifique en fonction de l'environnement
-const corsOptions = {
-    origin: '*', // Attention: Utiliser '*' seulement pour le débogage
+// Configuration CORS pour tester avec des en-têtes explicites
+app.use(cors({
+    origin: process.env.NODE_ENV === 'production' 
+            ? 'https://cv-web-site-client-1663cb36180a.herokuapp.com'
+            : 'http://localhost:3000',
     optionsSuccessStatus: 200,
     credentials: true,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     allowedHeaders: "Content-Type,Authorization"
-};
-
-app.use(cors(corsOptions));
+}));
 app.use(express.json());
+
+// Middleware pour ajouter des en-têtes CORS explicites
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    if (req.method === "OPTIONS") {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
+});
 
 // Log de débogage pour chaque requête
 app.use((req, res, next) => {
-    console.log(`Requête reçue avec méthode ${req.method} de ${req.origin} avec les en-têtes suivants: ${JSON.stringify(req.headers)}`);
+    console.log(`Requête reçue avec méthode ${req.method} à partir de ${req.originalUrl} avec les en-têtes suivants: ${JSON.stringify(req.headers)}`);
     next();
-});
-
-app.use((req, res, next) => {
-    res.header('Content-Type', 'application/json');
-    next();
-});
-
-app.get('/', (req, res) => {
-    res.json({ message: 'Hello from the backend!' });
 });
 
 app.use('/api/users', userRoutes);
 app.use('/api/stripe', stripeRoutes);
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello from the backend!' });
+});
 
 // Tester la connexion à la base de données avant de démarrer le serveur
 sequelize.authenticate()
